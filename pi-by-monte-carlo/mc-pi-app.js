5f919ec1baad058baea238b87762104af3ed6815
rbyirdaw <yrobelb@gmail.com>
2016-12-01 15:59:16 -0600

    start adding controller logic for user input to start/stop sim

--- pi-by-monte-carlo/mc-pi-app.js
+++ pi-by-monte-carlo/mc-pi-app.js
@@ -1,6 +1,6 @@
 
 angular.module("mcPiApp", [])
-    .controller("MCPiCtrl", function ($scope) {
+    .controller("MCPiCtrl", function($scope, $interval) {
 
       var mcPiModel = {
 	    x: undefined,
@@ -9,36 +9,55 @@ angular.module("mcPiApp", [])
 		hits: 0,
 		totalPoints: 0,
 
-		getRandomCoord: function () {
-		  return Math.floor(1 + Math.random()*(radius-1));
+		getRandomCoord: function() {
+		  return Math.floor(1 + Math.random()*(this.radius-1));
 		},
 
-		generatePoint: function () {
-		  x = getRanomCoord();
-		  y = getRanomCoord();
+		generatePoint: function() {
+		  x = this.getRandomCoord();
+		  y = this.getRandomCoord();
 
-		  totalPoints++;
+		  this.totalPoints++;
 
-		  if (isHit()) {
-			hits++;
+		  if (this.isHit()) {
+			this.hits++;
 		  }
 		},
 
-		isHit: function () {
-		  return (Math.pow(x,2) + Math.pow(y,2) <= Math.pow(radius,2));
+		isHit: function() {
+		  return (Math.pow(x,2) + Math.pow(y,2) <= Math.pow(this.radius,2));
 		}
 
 	  } //model		
 
 	  $scope.mcPi = mcPiModel;
+	  $scope.simRunning = false;
+	  var intervalId = undefined;
 
-	  $scope.startStopSim = function () {
+	  $scope.startStopSim = function() {
+		
+		if ($scope.simRunning) {
+		  $scope.simRunning = false;
+		  $interval.cancel(intervalId);
+		  intervalId = undefined;
+
+		} else {
+		  $scope.simRunning = true;
+		  intervalId = $interval(function() {
+			$scope.mcPi.generatePoint();
+		  }, 50);
+		}
 		$scope.dOut = "startStopSim";
 	  }
 
-	  $scope.clearSim = function () {
+	  $scope.clearSim = function() {
 		alert("stopSim");
 	  }
+
+
+	  
+
+	  
 
 	}); //controller
 		
