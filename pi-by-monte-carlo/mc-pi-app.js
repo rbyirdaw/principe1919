d250e8189f2886587242557febfcca71ba9cbd33
rbyirdaw <yrobelb@gmail.com>
2016-12-07 12:58:47 -0600

    directive watches model and updates canvas

--- pi-by-monte-carlo/mc-pi-app.js
+++ pi-by-monte-carlo/mc-pi-app.js
@@ -14,8 +14,8 @@ angular.module("mcPiApp", [])
 		},
 
 		generatePoint: function() {
-		  x = this.getRandomCoord();
-		  y = this.getRandomCoord();
+		  this.x = this.getRandomCoord();
+		  this.y = this.getRandomCoord();
 
 		  this.totalPoints++;
 
@@ -25,16 +25,22 @@ angular.module("mcPiApp", [])
 		},
 
 		isHit: function() {
-		  return (Math.pow(x,2) + Math.pow(y,2) <= Math.pow(this.radius,2));
+		  return (Math.pow(this.x,2) + Math.pow(this.y,2) <= Math.pow(this.radius,2));
 		},
 
 		getRadius: function() {
 		  return this.radius;
+		},
+
+		getTotalPoints: function() {
+		  return this.totalPoints;
 		}
 
 	  } //model		
 
 	  $scope.mcPi = mcPiModel;
+	  $scope.totalPoints = mcPiModel.totalPoints;
+
 	  $scope.simRunning = false;
 	  var intervalId = undefined;
 
@@ -49,6 +55,7 @@ angular.module("mcPiApp", [])
 		  $scope.simRunning = true;
 		  intervalId = $interval(function() {
 			$scope.mcPi.generatePoint();
+
 		  }, 50);
 		}
 		$scope.dOut = "startStopSim";
@@ -59,16 +66,27 @@ angular.module("mcPiApp", [])
 	  }
 
 
-	  
-
-	  
-
 	}) //controller
 
     .directive("simCanvas", function() {
 	  var canvasH,
-	      ctx;
+	      ctx,
+		  x,
+		  y,
+		  pointColor;
+	  
+	  function updateCanvas() {
 
+		ctx.strokeStyle = pointColor;
+
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		ctx.lineTo(x+1, y+1);
+		ctx.stroke();
+		
+		console.log("x "+x+", y "+y);
+	  }
+	  		
 	  function link(scope, element, attrs) {
 
 		element.append(angular.element("<canvas width='300' height='300' id='mcpi-canvas'>"));
@@ -82,6 +100,18 @@ angular.module("mcPiApp", [])
 		  ctx.strokeStyle = "tomato";
 		  ctx.stroke();
         }
+		
+		var watcherFunc = function() {
+		  return scope.mcPi.totalPoints;
+		}
+
+		scope.$watch(watcherFunc, function(newValue, oldValue) {
+	  	  scope.mcPi.isHit() ? pointColor = "#77c1c7" : pointColor = "#ff9900";
+		  x = scope.mcPi.x;
+		  y = scope.mcPi.y;
+
+		  updateCanvas();
+		});
 
       }//link
 
