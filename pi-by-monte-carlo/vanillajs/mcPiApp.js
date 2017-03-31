7fc9dd3d1e7f55f9905cf979ee4c9d6936460ac1
rbyirdaw <yrobelb@gmail.com>
2017-03-31 17:12:46 -0500

    controller and view logic, set up app module

---
+++ pi-by-monte-carlo/vanillajs/mcPiApp.js
@@ -0,0 +1,12 @@
+
+(function() {
+
+  var mcPiModel,
+      mcPiView,
+      mcPiController;
+
+  mcPiView = new mcPiApp.View();
+  mcPiController = new mcPiApp.Controller(mcPiModel, mcPiView);  
+
+
+})();
