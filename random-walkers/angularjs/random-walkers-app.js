d72c4f52fd4361dc7e442113655b8d7d50b1ad1d
rbyirdaw <yrobelb@gmail.com>
2018-03-22 19:46:41 -0500

    added model, setting canvas directive, controller init

--- random-walkers/angularjs/random-walkers-app.js
+++ random-walkers/angularjs/random-walkers-app.js
@@ -1,7 +1,25 @@
 angular.module("randomWalkersApp", [])
-  .controller("RandomWalkersCtrl", function() {
+  .controller("RandomWalkersCtrl", function($scope, $interval) {
+
+    $scope.rwModel = new RandomWalkModel(200, 250, 150);
+    $scope.simRunning = false;
+
+    $scope.initSim = function() {
+      $scope.rwModel.init();
+    };
 
   })
-  .directive("", function() {
+  .directive("simCanvas", function() {
+    var canvasH, ctx;
+
+    function link(scope, element, attrs) {
+      element.append(angular.element("<canvas width='500' height='300'>"));
+      canvasH = element.find("canvas");
+      ctx = canvasH[0].getContext("2d");      
+    } // link
+
+    return {
+      link: link
+    }
 
   });
